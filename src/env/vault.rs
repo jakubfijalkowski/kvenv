a288d58c91925b922f3e031eed0f2d3284db72b5
Jakub Fijałkowski <kuba@codinginfinity.me>
2022-12-03 23:45:44 +0100

    Add self-signed CA certificate support to Vault

--- src/env/vault.rs
+++ src/env/vault.rs
@@ -1,10 +1,10 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::PathBuf};
 
 use clap::{arg, command, ArgGroup, Args};
-use futures::TryFutureExt;
 use reqwest::{self, StatusCode};
 use serde::Deserialize;
 use thiserror::Error;
+use tokio::io::AsyncReadExt;
 
 use super::{convert::as_valid_env_name, Vault, VaultConfig};
 
@@ -12,6 +12,10 @@ use super::{convert::as_valid_env_name, Vault, VaultConfig};
 #[command(group = ArgGroup::new("hashicorp"))]
 pub struct HashicorpVaultConfig {
     /// Use Hashicorp Vault.
+    /// Vault mode works differently to other clouds. When "single secret" mode is selected, it
+    /// interprets the document as a key-value document, where key is the environment variable
+    /// name.
+    /// When in perfixed mode, it uses the name of matched secret as a environment variable name.
     #[arg(
         name = "vault",
         long = "vault",
@@ -28,6 +32,9 @@ pub struct HashicorpVaultConfig {
     /// [Hashicorp Vault] Token that should be used to authorize the request.
     #[arg(long, env = "VAULT_TOKEN", hide_env_values = true, display_order = 402)]
     vault_token: Option<String>,
+
+    #[arg(long, value_parser, env = "VAULT_CACERT", display_order = 403)]
+    vault_cacert: Option<PathBuf>,
 }
 
 #[derive(Error, Debug)]
@@ -42,7 +49,7 @@ pub enum HashicorpVaultError {
     ForbiddenError(String),
 
     #[error("HTTP error occurred")]
-    HttpError(#[from] reqwest::Error),
+    HttpError(#[source] reqwest::Error),
 
     #[error("the Vault returned non-200 error code")]
     HttpStatusCodeError(StatusCode),
@@ -52,11 +59,15 @@ pub enum HashicorpVaultError {
 
     #[error("the keys in the secret are not valid env names")]
     InvalidEnv(#[source] anyhow::Error),
+
+    #[error("the configuration is invalid")]
+    ConfigurationError(#[from] anyhow::Error),
 }
 
 pub struct HashicorpVault {
     address: String,
     token: String,
+    cacert: Option<PathBuf>,
 }
 
 impl VaultConfig for HashicorpVaultConfig {
@@ -70,7 +81,33 @@ impl VaultConfig for HashicorpVaultConfig {
         Ok(Self::Vault {
             address: self.vault_address.unwrap(),
             token: self.vault_token.unwrap(),
+            cacert: self.vault_cacert,
         })
+    }
+}
+
+impl HashicorpVault {
+    async fn client(&self) -> Result<reqwest::Client, HashicorpVaultError> {
+        let mut builder = reqwest::Client::builder().user_agent("kvenv");
+
+        if let Some(path) = self.cacert.as_ref() {
+            let mut buffer = Vec::new();
+            {
+                let mut file = tokio::fs::File::open(path)
+                    .await
+                    .map_err(anyhow::Error::new)?;
+                file.read_to_end(&mut buffer)
+                    .await
+                    .map_err(anyhow::Error::new)?;
+            }
+            let cert = reqwest::Certificate::from_pem(&buffer).map_err(anyhow::Error::new)?;
+            builder = builder.add_root_certificate(cert);
+        }
+
+        builder
+            .build()
+            .map_err(anyhow::Error::new)
+            .map_err(HashicorpVaultError::ConfigurationError)
     }
 }
 
@@ -81,7 +118,7 @@ impl Vault for HashicorpVault {
 
     #[tokio::main]
     async fn download_json(&self, secret_name: &str) -> anyhow::Result<Vec<(String, String)>> {
-        let client = reqwest::Client::new();
+        let client = self.client().await?;
         let response = client
             .get(format!("{}/v1/secret/data/{}", self.address, secret_name))
             .header("X-Vault-Token", &self.token)
